89f9b593c4a51655b3a538ebde6f99aa8787fe8a
birgitmelissa <94128573+birgitmelissa@users.noreply.github.com>
2021-12-18 19:37:01 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+//Part 1:
+
+var age = 38;
+
+if (age >= 18) {
+    console.log("You are alowed to go in");
+} else {
+    console.log("You are to young to go in, please stay outside");
+}
+
+//Part 2:
+
+var isFemale = false;
+
+if (isFemale) {
+    console.log("You can buy a ticket");
+} else {
+    console.log("You can't buy a ticket");
+}
+
+//Part 3:
+
+var driverStatus = 'bob' 
+
+if (driverStatus = 'bob') {
+    console.log("You are allowed to drive");
+} else {
+    console.log("You are not allowed to drive");
+}
